42310ef2d514bc28a49d7b2173140e39c6e821a6
Soowan Choi <soowan@Soowans-MacBook-Air.local>
2023-06-15 10:19:06 -0400

    Visualize Objective 2

---
+++ OB2_visuals.py
@@ -0,0 +1,128 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from sklearn.metrics import confusion_matrix
+
+
+
+df = pd.read_csv("/Users/soowan/Downloads/data_visuals.csv")
+
+
+# Data points
+x = df['Sensitivity']
+y = df['Specificity']
+z = df['Precision']
+f1 = df['F1-Score']
+acc = df['Accuracy']
+labels = df['Game ID']
+
+
+
+# ROC Curve (Sensitivity vs 1 - Specificity)
+plt.scatter(1 - y, x)
+
+# Add labels to the data points
+for i, label in enumerate(labels):
+    plt.annotate(label, (1-y[i], x[i]), textcoords="offset points", xytext=(0,10), ha='center', fontsize = 10)
+
+# Add axis labels and a title
+plt.xlabel('False Positive Rate (1 - Specificity)')
+plt.ylabel('True Positive Rate (Sensitivity/Recall)')
+plt.title('ROC Curve for Exercise')
+
+# Display the plot
+plt.show()
+
+
+
+
+
+# PRECISION-RECALL CURVE
+plt.figure()
+# Create the scatter plot
+scatter = plt.scatter(x, z)
+
+# Add labels to the data points
+for i, label in enumerate(labels):
+    plt.annotate(label, (x[i], z[i]), textcoords="offset points", xytext=(0,10), ha='center', fontsize = 10)
+
+# Add axis labels and a title
+plt.xlabel('Sensitivity/Recall')
+plt.ylabel('Precision')
+plt.title('Precision-Recall for Exercise')
+
+# Display the plot
+plt.show()
+
+
+
+
+
+# BUBBLE CHART: SENSITIVITY VS SPECIFICITY VS PRECISION
+plt.scatter(x, y, s=np.array(z)*500, alpha=0.5)
+
+# Add labels to the data points
+for i, label in enumerate(labels):
+    plt.annotate(label, (x[i], y[i]), textcoords="offset points", xytext=(0,10), ha='center', fontsize = 10)
+
+# Add axis labels and a title
+plt.xlabel('Sensitivity/Recall')
+plt.ylabel('Specificity')
+plt.title('Specificity vs Sensitivity (Bubble Size: Precision)')
+
+# Display the plot
+plt.show()
+
+
+
+
+
+
+# F1-Score vs Exercise
+plt.figure()
+# Create the scatter plot
+scatter = plt.bar(labels, f1)
+
+# Add labels to the data points
+for i, label in enumerate(labels):
+    plt.annotate(label, (labels[i], f1[i]), textcoords="offset points", xytext=(0,10), ha='center', fontsize = 10)
+
+# Add axis labels and a title
+plt.xlabel('Exercise')
+plt.ylabel('F1-Score')
+plt.title('F1-Score for Exercise')
+
+# Display the plot
+plt.show()
+
+
+
+
+
+
+# Accuracy vs Exercise
+plt.figure()
+# Create the scatter plot
+scatter = plt.bar(labels, acc)
+
+# Add labels to the data points
+for i, label in enumerate(labels):
+    plt.annotate(label, (labels[i], acc[i]), textcoords="offset points", xytext=(0,10), ha='center', fontsize = 10)
+
+# Add axis labels and a title
+plt.xlabel('Exercise')
+plt.ylabel('Accuracy [%]')
+plt.title('Accuracy for Exercise')
+
+# Display the plot
+plt.show()
+
+
+
+
+
+
+
+
+
+
